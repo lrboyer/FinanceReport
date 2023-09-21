90c6b4a7a715a094c5be976778b29198a7ab8b71
Lucas Boyer <lrboyer@clemson.edu>
2023-09-21 13:28:53 -0400

    added query parameter

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function FileUpload() {
+  const auth_id = "bruh12345"
   const [file, setFile] = useState(null);
   const [pass, setPass] = useState("");
 
@@ -12,52 +13,51 @@ function FileUpload() {
 
   // Function to handle file input change
   const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
+    setFile(event.target.files[0]);
   };
 
   // Function to handle file upload and send PUT request
   const handleUpload = async () => {
     if (file && pass) {
+      try {
+        const formData = new FormData();
+        formData.append('file', file);
 
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
+        const response = await axios.post('https://twt87y3iyj.execute-api.us-east-1.amazonaws.com/staging', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          params: {
+            authId: auth_id,
+          },
+        });
 
-      // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
-      const response = await axios.post('https://twt87y3iyj.execute-api.us-east-1.amazonaws.com/staging', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'AuthId': pass
-        },
-      });
-
-      // Handle the response from the API here
-      console.log('Upload successful:', response.data);
-    } catch (error) {
-      console.error('Error uploading file:', error);
+        // Handle the response from the API here
+        console.log('Upload successful:', response.data);
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
+    } else {
+      alert('Please select a file before uploading.');
     }
-  } else {
-    alert('Please select a file before uploading.');
-}
   };
 
-return (
-  <div>
-    <h2>File Upload</h2>
-    <input type="file" onChange={handleFileChange} />
-    <>
-      <span>Password</span>
-      <input
-        type="text"
-        name="pass"
-        value={pass}
-        onChange={handleTextChange}
-      />
-    </>
-    <button onClick={handleUpload}>Upload</button>
-  </div>
-);
+  return (
+    <div>
+      <h2>File Upload</h2>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <>
+        <span>Password</span>
+        <input
+          type="text"
+          name="pass"
+          value={pass}
+          onChange={handleTextChange}
+        />
+      </>
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
 }
 
 export default FileUpload;
