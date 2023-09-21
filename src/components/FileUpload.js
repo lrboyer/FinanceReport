9a1335d741b34b952b63bb62b427bec0b1d01fa3
Lucas Boyer <lrboyer@clemson.edu>
2023-09-21 16:10:51 -0400

    changed to get signed url back

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 function FileUpload() {
   const [file, setFile] = useState(null);
   const [pass, setPass] = useState("");
+  const password = "bruh12345"
 
   const handleTextChange = (event) => {
     let selectedText = event.target.value;
@@ -18,23 +19,27 @@ function FileUpload() {
   // Function to handle file upload and send PUT request
   const handleUpload = async () => {
     if (file && pass) {
-      try {
-        const formData = new FormData();
-        formData.append('file', file);
+      if (pass === password) {
+        try {
+          const body = {
+            "fileName": file.name,
+            "fileType": "application/csv"
+          }
 
-        const response = await axios.post('https://twt87y3iyj.execute-api.us-east-1.amazonaws.com/staging', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          },
-          params: {
-            authId: pass,
-          },
-        });
+          const response = await axios.post('https://twt87y3iyj.execute-api.us-east-1.amazonaws.com/staging', body, {
+            headers: {
+              'Content-Type': 'application/json',
+              'x-api-key': 'N017qwyGaI2I5LkUOtQcn2QsDqbQAOY04PFRji7V'
+            }
+          });
 
-        // Handle the response from the API here
-        console.log('Upload successful:', response.data);
-      } catch (error) {
-        console.error('Error uploading file:', error);
+          // Handle the response from the API here
+          console.log('Upload successful:', response.data);
+        } catch (error) {
+          console.error('Error uploading file:', error);
+        }
+      } else {
+        alert("Wrong Password")
       }
     } else {
       alert('Please select a file before uploading.');
